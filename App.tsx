522c97133252705dcb11d1f8d8c0d65cfd72c0da
Seborider <mayer.sebobo@gmail.com>
2025-08-29 17:45:24 +0200

    Add temporary Reanimated logging blocker

--- App.tsx
+++ App.tsx
@@ -4,6 +4,17 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { AppNavigator } from './src/navigation/AppNavigator';
 
+import {
+  configureReanimatedLogger,
+  ReanimatedLogLevel,
+} from 'react-native-reanimated';
+
+// This is the default configuration
+configureReanimatedLogger({
+  level: ReanimatedLogLevel.warn,
+  strict: false, // Reanimated runs in strict mode by default
+});
+
 function App(): React.JSX.Element {
   return (
     <GestureHandlerRootView style={styles.container}>
